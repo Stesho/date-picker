5471cb507b7b6c569198b23ca3e53b01abea5b61
Steshenko Dmitry <3301297@gmail.com>
2024-01-22 21:53:11 +0300

    debug: add logs

--- src/api/fetchHolidays.ts
+++ src/api/fetchHolidays.ts
@@ -1,8 +1,10 @@
 import { holidaysInstance } from '@/api/config';
+import { HOLIDAYS_API_KEY } from '@/constants/environment/environment';
 import { ALL_HOLIDAYS_URL } from '@/constants/holidays/endpoints';
 import { Holiday } from '@/types/Holiday';
 
 export const fetchHolidays = async (country: string, year: number) => {
+  console.log('fetchHolidays:', HOLIDAYS_API_KEY);
   const received: Holiday[] = await holidaysInstance.get(
     `${ALL_HOLIDAYS_URL}`,
     {
--- src/constants/environment/environment.ts
+++ src/constants/environment/environment.ts
@@ -1,1 +1,5 @@
+console.log('environment:', process);
+console.log('environment:', process.env);
+console.log('environment:', process.env.HOLIDAYS_API_KEY);
+
 export const HOLIDAYS_API_KEY = process.env.HOLIDAYS_API_KEY || '';

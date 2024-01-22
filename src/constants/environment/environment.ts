41ae4ba3ef6c00b372ccccbaf80780ef73d83bd0
Steshenko Dmitry <3301297@gmail.com>
2024-01-22 22:13:56 +0300

    fix: add env to the storybook config

--- src/constants/environment/environment.ts
+++ src/constants/environment/environment.ts
@@ -1,5 +1,5 @@
 console.log('environment:', process);
-console.log('environment:', process.env);
+console.log('environment:', JSON.stringify(process.env));
 console.log('environment:', process.env.HOLIDAYS_API_KEY);
 
 export const HOLIDAYS_API_KEY = process.env.HOLIDAYS_API_KEY || '';

a6da63b43061206f76d2b389e1232b30424a7310
Steshenko Dmitry <3301297@gmail.com>
2024-01-22 23:15:39 +0300

    feat: add static holidays data

--- src/api/fetchHolidays.ts
+++ src/api/fetchHolidays.ts
@@ -3,24 +3,26 @@ import { ALL_HOLIDAYS_URL, BASE_URL } from '@/constants/holidays/endpoints';
 import { Holiday } from '@/types/Holiday';
 
 export const fetchHolidays = async (country: string, year: number) => {
-  const searchParams = new URLSearchParams({
-    type: 'major_holiday',
-    country: country.toString(),
-    year: year.toString(),
-  });
+  try {
+    const searchParams = new URLSearchParams({
+      type: 'major_holiday',
+      country: country.toString(),
+      year: year.toString(),
+    });
 
-  const holidays = await fetch(
-    `${BASE_URL}${ALL_HOLIDAYS_URL}?${searchParams}`,
-    {
-      headers: {
-        Accept: 'text/json',
-        'X-Api-Key': HOLIDAYS_API_KEY,
-        'Cache-Control': 'no-cache',
-        Pragma: 'no-cache',
-        Expires: '0',
+    const holidays = await fetch(
+      `${BASE_URL}${ALL_HOLIDAYS_URL}?${searchParams}`,
+      {
+        headers: {
+          Accept: 'text/json',
+          'X-Api-Key': HOLIDAYS_API_KEY,
+        },
       },
-    },
-  );
+    );
 
-  return (await holidays.json()) as Promise<Holiday[]>;
+    return await ((await holidays.json()) as Promise<Holiday[]>);
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
 };

5afcc403cbf51b382738dc7966fa5587da0eefbd
Steshenko Dmitry <3301297@gmail.com>
2024-01-23 03:06:22 +0300

    fix: delete axios

--- src/constants/environment/environment.ts
+++ src/constants/environment/environment.ts
@@ -1,5 +1,1 @@
-console.log('environment:', process);
-console.log('environment:', JSON.stringify(process.env));
-console.log('environment:', process.env.HOLIDAYS_API_KEY);
-
 export const HOLIDAYS_API_KEY = process.env.HOLIDAYS_API_KEY || '';
